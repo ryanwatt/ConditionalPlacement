629de3c0c4c3248259800ed23d7f69b859bd2060
Ryan Watt <ryanwatt24@gmail.com>
2015-02-02 13:26:11 -0700

    my last commit

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -18,9 +18,23 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    int karl = 10;
+    int leonard = 7;
+    int karlPlace = karl > 7 ? 1 : 2;
+    int leonardPlace = leonard > 7 ? 1 : 2;
     
-    
+    if (karlPlace == 1 && leonardPlace == 1){
+        NSLog(@"Both teachers are placed 1\n");
+    }
+    else if (karlPlace == 1 && leonardPlace == 2){
+        NSLog(@"Karl is placed 1 and Leonard is placed 2\n");
+    }
+    else if (leonardPlace == 1 && karlPlace == 2){
+        NSLog(@"Leonard is placed 1 and Karl is placed 2\n");
+    }
+    else{
+        NSLog(@"Both teachers are placed 2\n");
+    }
     return YES;
 }
-
 @end
